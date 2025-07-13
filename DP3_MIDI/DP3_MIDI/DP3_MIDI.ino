c927f2d37b93ecb7a1ed179fa326f747aec0ab25
RVR11pitt <rvr11@pitt.edu>
2025-07-13 17:58:33 -0400

    Added main file and containment folder
    Main file is DP3_MIDI.ino and the folder that will contain all the ino files will be named the same to ensure DP3_MIDI is compiled as the main file.

---
+++ DP3_MIDI/DP3_MIDI/DP3_MIDI.ino
@@ -0,0 +1,19 @@
+/*
+  This is the main code for ECE 1895, Design Project 3. It contains the operating
+  code for running a simple MIDI controller that interfaces with a digital audio
+  workstation (DAW) through analog dials and sliders, in additional to buttons
+  and other switches for various purposes.
+
+
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
